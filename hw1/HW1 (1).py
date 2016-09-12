fd026e36cdc04a62b26db6d1d93b6f49f2014714
Abhinav SInha <a.s.rex2006@gmail.com>
2016-09-12 16:29:27 -0500

    Created 3D array as required with range 0-19

--- hw1/HW1 (1).py
+++ hw1/HW1 (1).py
@@ -2,3 +2,9 @@ import scipy
 import pylab
 from scipy.spatial.distance import squareform, pdist, cdist
 import matplotlib.pylab as plt
+
+# Creating a 3D array containing the coordinates (x,y) from the arrays X and Y
+
+points = scipy.mgrid[0:20, 0:20]
+coords = points.reshape(2, -1).T
+
