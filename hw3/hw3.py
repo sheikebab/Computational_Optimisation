5030e532a95a59a786adbda82991de9d269f9649
Abhinav SInha <a.s.rex2006@gmail.com>
2016-09-21 08:21:16 -0500

    Create and initialise the Parabola class

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,3 +1,12 @@
 import scipy
 import matplotlib.pyplot as plt
 import pandas
+class Parabola:
+
+    # '''Defines  a  function  that  looks  like:
+    #
+    # f(x) = Summ over i (alpha_i(x_i-c_i)^2) '''
+
+    def __init__(self, alpha, center = 0):
+        self.alpha = alpha
+        self.center = center
